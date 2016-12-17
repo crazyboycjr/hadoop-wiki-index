a98fac3daa6a4fb7126688a132f4003c24ced174
crazyboycjr <crazyboycjr@gmail.com>
2016-12-17 13:11:27 +0800

    calculate DF at the same time

--- cxx/offtable.cc
+++ cxx/offtable.cc
@@ -1,6 +1,6 @@
 /*
  * This Program preprocess the offset of each line in inverted-index-fmt.txt
- * for query use.
+ * for query use, also we can calculate DF here.
  */
 #include <cstdio>
 #include <cstring>
@@ -16,11 +16,16 @@ int main() {
 	char c;
 	long off = 0;
 	while (1) {
-		while (off++, (c = getchar()) != '\n' && c > 0);
+		long df = 0;
+		while (off++, (c = getchar()) != '\n' && c > 0)
+			df += c == ' ';
 		if (c < 0)
 			break;
-		scanf("%s", s);
-		printf("%s %d\n", s, off);
+		if (df)
+			printf(" %ld\n", df);
+		if (scanf("%s", s) == EOF)
+			break;
+		printf("%s %ld", s, off);
 		off += strlen(s);
 	}
 	return 0;
